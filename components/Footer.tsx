0a96044888fffc150a427f4cf734e60086f8c49a
Gerald Shimo <shimogerald450@gmail.com>
2025-12-19 00:27:05 -0500

    v3.1.0

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="bg-gray-900 text-white py-8">
       <div className="container-custom text-center">
         <p className="text-gray-400">
-          @2024 Gerald Shimo | v1.0.0
+          @2024 Gerald Shimo | v3.1.0
         </p>
       </div>
     </footer>
